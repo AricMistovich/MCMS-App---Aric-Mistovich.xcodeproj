3a28354e2bc1a5e7f666d48bbc66b118ad5f700d
AricMistovich <51251879+AricMistovich@users.noreply.github.com>
2019-05-31 18:55:46 -0700

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MCMS App - Aric Mistovich
+//
+//  Created by Aric Mistovich on 5/31/19.
+//  Copyright © 2019 Aric Mistovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
